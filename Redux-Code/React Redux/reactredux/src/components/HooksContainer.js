b9b9281bc45506c1a26508c8e3bc17c92e863a4c
aayush <kaayush163@gmail.com>
2025-08-15 22:49:52 +0530

    Redux concepts covered

---
+++ Redux-Code/React Redux/reactredux/src/components/HooksContainer.js
@@ -0,0 +1,16 @@
+import { useSelector, useDispatch } from "react-redux";
+import { orderPizza } from "./redux"; //in simple meaning it is imported from index.js
+function HookContainer() {
+  const pizzaBase = useSelector((state) => state.pizzaBase);
+  const dispatch = useDispatch();
+  return (
+    <div className="container">
+      <h2 className="text">Number of Pizza Base available - {pizzaBase}</h2>
+      <button className="btn" onClick={() => dispatch(orderPizza())}>
+        Order Pizza
+      </button>
+    </div>
+  );
+}
+
+export default HookContainer;
